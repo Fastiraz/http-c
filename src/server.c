58e9e8bef989f8427c88b8901678f5931b207dc3
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2025-02-25 22:59:44 +0100

    feat: v0.0.1-pre-alpha
    functionnal http server in c!

--- src/server.c
+++ src/server.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
 #include <unistd.h>
 
 #define PORT 8000
@@ -12,14 +11,14 @@
 #define warn(msg, ...) printf("\x1b[48;5;235m\x1b[38;5;215m\x1b[1m[*]\x1b[0m " msg "\n", ##__VA_ARGS__)
 #define info(msg, ...) printf("\x1b[48;5;235m\x1b[38;5;117m\x1b[1m[i]\x1b[0m " msg "\n", ##__VA_ARGS__)
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[], char *envp[]) {
   if (argc != 1)
     return EXIT_FAILURE;
 
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   okay("Socket created successfully");
 
-  struct sockaddr_in server;
+  struct sockaddr_in server, client;
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = htonl(INADDR_ANY);
   server.sin_port = htons(PORT);
@@ -29,6 +28,24 @@ int main(int argc, char *argv[]) {
 
   listen(socket_fd, 3);
   info("Listening on port %d", PORT);
+
+  while (1) {
+    socklen_t client_len = sizeof(client);
+    accept(socket_fd, (struct sockaddr *)&client, &client_len);
+    okay("New connection accepted from %s:%d", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+    char response[] =
+      "HTTP/1.1 200 OK\r\n"
+      "Content-Type: text/plain\r\n"
+      "Content-Length: 13\r\n"
+      "\r\n"
+      "HTTP from scratch!";
+
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    send(client_fd, response, sizeof(response) - 1, 0);
+    close(client_fd);
+    info("Connection closed");
+  }
 
   close(socket_fd);
   return EXIT_SUCCESS;
